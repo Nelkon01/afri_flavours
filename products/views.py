e22b385f1edb880638431e9d5fb2581df39607bb
Oluwadamilola Oyenekan <adeniyioyenekan@yahoo.co.uk>
2024-02-08 13:32:59 +0000

    Create the products page and the functionality for the page

--- products/views.py
+++ products/views.py
@@ -2,17 +2,53 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
+from django.db.models.functions import Lower
 
 from .models import Product, Category, Allergen, Review
+from .forms import ProductForm
 
 
 # Create your views here.
 
 def all_products(request):
+    """ A view to show all products, including sorting and search queries"""
     products = Product.objects.all()
     query = None
     categories = None
     sort = None
     direction = None
 
-    return render(request)
+    if request.GET:
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+
+        queries = Q(name__icontains=query) | Q(description__icontains=query)
+        products = products.filter(queries)
+
+    current_sorting = f'{sort}_{direction}'
+
+    context = {
+        'products': products,
+        'search_term': query,
+        'current_categories': categories,
+        'current-sorting': current_sorting,
+    }
+
+    return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """ A view to show individual product details"""
+    product = get_object_or_404(Product, pk=product_id)
+    reviews = Review.objects.filter(product=product)
+
+    context = {
+        'product': product,
+        'reviews': reviews,
+    }
+
+    return render(request, 'products/product_detail.html', context)
