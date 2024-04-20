5b76efb89b38a95f12b8c2106c3fd82ae2da383d
Oluwadamilola Oyenekan <adeniyioyenekan@yahoo.co.uk>
2024-04-20 16:24:44 +0100

    Include functionality for users to search for products using category links in nav

--- products/views.py
+++ products/views.py
@@ -11,7 +11,7 @@ from .forms import ProductForm
 # Create your views here.
 
 def all_products(request):
-    """ A view to show all products, including sorting and search queries"""
+    """ A view to show all products, including sorting and search queries """
     products = Product.objects.all()
     query = None
     categories = None
@@ -19,44 +19,43 @@ def all_products(request):
     direction = None
 
     if request.GET:
+        # Handling sorting
+        sortkey = request.GET.get('sort')
+        direction = request.GET.get('direction', '')
 
-        if 'sort' in request.GET:
-            sortkey = request.GET['sort']
+        if sortkey:
             sort = sortkey
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-            if sortkey == 'category':
-                sortkey = 'category__name'
-            if sortkey == 'allergens':
-                sortkey = 'allergens__name'
-            if 'direction' in request.GET:
-                direction = request.GET['direction']
-                if direction == 'desc':
-                    sortkey = f'-{sortkey}'
+            elif sortkey in ['category', 'allergens']:
+                sortkey = f'{sortkey}__name'
+
+            if direction == 'desc':
+                sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
 
-        if 'category' in request.GET:
-            categories = request.GET['category'].split(',')
-            products = products.filter(category__name__in=categories)
-            categories = Category.objects.filter(name__in=categories)
+        # Handling category filtering
+        categories = request.GET.get('category')
+        if categories:
+            category_list = categories.split(',')
+            products = products.filter(category__name__in=category_list)
+            categories = Category.objects.filter(name__in=category_list)
 
-        if 'q' in request.GET:
-            query = request.GET['q']
-            if not query:
-                messages.error(request, "You didn't enter any search criteria!")
-                return redirect(reverse('products'))
-
-        queries = Q(name__icontains=query) | Q(description__icontains=query)
-        products = products.filter(queries)
-
-    current_sorting = f'{sort}_{direction}'
+        # Handling search queries
+        query = request.GET.get('q')
+        if query:
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+        elif 'q' in request.GET:
+            messages.error(request, "You didn't enter any search criteria!")
+            return redirect(reverse('products'))
 
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
-        'current-sorting': current_sorting,
+        'current_sorting': f'{sort}_{direction}',
     }
 
     return render(request, 'products/products.html', context)
