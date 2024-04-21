a5070aa71062c9de86c4ba73200f8f2e8ff5228f
Oluwadamilola Oyenekan <adeniyioyenekan@yahoo.co.uk>
2024-04-21 19:23:04 +0100

    Update the settings file and fix checkout page error not working

--- afri_flavours/settings.py
+++ afri_flavours/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
+import decouple
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -179,5 +182,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Stripe
 STRIPE_CURRENCY = 'usd'
-STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
-STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_PUBLIC_KEY = decouple.config('STRIPE_PUBLIC_KEY')
+STRIPE_SECRET_KEY = decouple.config('STRIPE_SECRET_KEY')
+STRIPE_WH_SECRET = decouple.config('STRIPE_WH_SECRET')
