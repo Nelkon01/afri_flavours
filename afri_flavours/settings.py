11108f51b6a2adae29c8fc910511c4c549953e5e
Oluwadamilola Oyenekan <adeniyioyenekan@yahoo.co.uk>
2024-05-02 22:05:04 +0100

    Made changes for deployment

--- afri_flavours/settings.py
+++ afri_flavours/settings.py
@@ -24,6 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = decouple.config('SECRET_KEY')
 SECRET_KEY = 'django-insecure-s&bf^6pkj$yne=ll=d^=ck^dud@nfad2+2x!=eiwe1-+&u80u^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
