e34dba1bb623422f47f474542090aff213d20cdf
Oluwadamilola Oyenekan <adeniyioyenekan@yahoo.co.uk>
2024-05-04 01:03:28 +0100

    Deployment update

--- afri_flavours/settings.py
+++ afri_flavours/settings.py
@@ -191,8 +191,8 @@ if 'USE_AWS' in os.environ:
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'afri-flavours-0342f46728dc'
     AWS_S3_REGION_NAME = 'eu-west-1'
-    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and Media files
