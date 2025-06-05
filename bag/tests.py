fa1d6b0702a62e34e9785cf0d4d41fce44ba9fa4
Dami Oyenekan <54297166+Nelkon01@users.noreply.github.com>
2025-06-05 15:15:07 +0100

    Update deployment config

--- bag/tests.py
+++ bag/tests.py
@@ -1,1 +1,45 @@
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+
+from products.models import Product, Category
+
+
+class BagViewsTests(TestCase):
+    def setUp(self):
+        category = Category.objects.create(name="snack")
+        self.product = Product.objects.create(name="Chips", description="crispy", price=1, category=category)
+
+    def test_add_adjust_and_remove_bag(self):
+        add_url = reverse("add_to_bag", args=[self.product.id])
+        adjust_url = reverse("adjust_bag", args=[self.product.id])
+        remove_url = reverse("remove_from_bag", args=[self.product.id])
+
+        # add
+        response = self.client.post(add_url, {"quantity": 2, "redirect_url": reverse("products")})
+        self.assertEqual(self.client.session["bag"].get(str(self.product.id)), 2)
+        self.assertEqual(response.status_code, 302)
+
+        # adjust
+        response = self.client.post(adjust_url, {"quantity": 1})
+        self.assertEqual(self.client.session["bag"].get(str(self.product.id)), 1)
+        self.assertEqual(response.status_code, 302)
+
+        # remove
+        response = self.client.post(remove_url)
+        self.assertNotIn(str(self.product.id), self.client.session.get("bag", {}))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_bag_template(self):
+        url = reverse("view_bag")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "bag/bag.html")
+
+    def test_bag_contents_context(self):
+        add_url = reverse("add_to_bag", args=[self.product.id])
+        self.client.post(add_url, {"quantity": 3, "redirect_url": reverse("products")})
+        from bag.contexts import bag_contents
+        request = self.client.get(reverse("view_bag")).wsgi_request
+        context = bag_contents(request)
+        self.assertEqual(context["product_count"], 3)
+        self.assertTrue(context["bag_items"])
--- blog/tests.py
+++ blog/tests.py
@@ -1,1 +1,34 @@
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+from .models import Post, Comment
+
+
+class BlogModelTests(TestCase):
+    def test_post_and_comment_str(self):
+        user = User.objects.create(username="author")
+        post = Post.objects.create(title="Title", author=user, body="Body", status=Post.Status.PUBLISHED)
+        comment = Comment.objects.create(author="Anon", body="hi", post=post)
+        self.assertEqual(str(post), "Title")
+        self.assertIn("Anon", str(comment))
+
+
+class BlogViewsTests(TestCase):
+    def setUp(self):
+        user = User.objects.create(username="author")
+        self.post = Post.objects.create(title="A", author=user, body="b", status=Post.Status.PUBLISHED)
+
+    def test_post_list_view(self):
+        url = reverse("blog:post_list")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "blog/post_list.html")
+        self.assertIn(self.post, response.context["posts"])
+
+    def test_post_detail_view(self):
+        url = reverse("blog:post_detail", args=[self.post.id])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "blog/post_detail.html")
+        self.assertEqual(response.context["object"], self.post)
