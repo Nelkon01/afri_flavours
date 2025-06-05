866f47668de97bd17b5423237215a5f233b5049f
Dami Oyenekan <54297166+Nelkon01@users.noreply.github.com>
2025-06-05 12:53:26 +0100

    Add unit tests for core apps

--- home/tests.py
+++ home/tests.py
@@ -1,1 +1,9 @@
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+
+
+class HomeViewTests(TestCase):
+    def test_index_view(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home/index.html')
